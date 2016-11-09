b467234bf1d89e4e51f44ae3f546f1cfe175a4a3
Jorge García <jgarciao@gmail.com>
2016-11-09 23:17:47 +0100

    Implement the Install button in AppDetailsComponent

--- src/app/app-details/app-details.component.ts
+++ src/app/app-details/app-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
-import { Http, Headers, RequestOptions, ResponseContentType } from '@angular/http';
+import { Http, BrowserXhr, Headers, RequestOptions, ResponseContentType } from '@angular/http';
 import { Observable } from 'rxjs';
+import { saveAs } from "file-saver";
 
 import { App } from '../shared/app.model';
 import { FlathubApiService } from '../flathub-api.service';
@@ -16,6 +17,8 @@ export class AppDetailsComponent implements OnInit {
 
   @Input()
   app: App;
+
+  public pending:boolean = false;
 
   constructor(
     private http: Http,
@@ -42,15 +45,43 @@ export class AppDetailsComponent implements OnInit {
     this.location.back();
   }
 
-  install() {
+  // This method has been adapted from Stack Overflow
+  // Question: http://stackoverflow.com/questions/35368633/angular-2-download-pdf-from-api-and-display-it-in-view
+  // Answer by spock: http://stackoverflow.com/users/435743/spock
 
+  public install() {
 
-    /*var blob = new Blob(["Hello, world!"], { type: "text/plain;charset=utf-8" });
-    saveAs(blob, "hello world.txt");
+        // Xhr creates new context so we need to create reference to this
+        let self = this;
 
+        // Status flag used in the template.
+        this.pending = true;
 
-    let blob: Blob = res.blob();
-    window['saveAs'](blob, 'test.pdf');*/
-  }
+        // Create the Xhr request object
+        let xhr = new XMLHttpRequest();
+        let url =  this.app.flatpakRefUrl;
+
+        xhr.open('GET', url, true);
+        xhr.responseType = 'blob';
+
+        // Xhr callback when we get a result back
+        // We are not using arrow function because we need the 'this' context
+        xhr.onreadystatechange = function() {
+
+            // We use setTimeout to trigger change detection in Zones
+            setTimeout( () => { self.pending = false; }, 0);
+
+            // If we get an HTTP status OK (200), save the file using fileSaver
+            if(xhr.readyState === 4 && xhr.status === 200) {
+                var blob = new Blob([this.response], {type: 'application/vnd.flatpak.ref'});
+                var filename: string  = self.app.flatpakRefUrl.substring(url.lastIndexOf('/')+1);
+                saveAs(blob, filename);
+            }
+        };
+
+        // Start the Ajax request
+        xhr.send();
+    }
+
 
 }
