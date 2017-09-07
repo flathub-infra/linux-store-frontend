70d5736e855ed3d3f42f9a7ce5817e29e5ea8ab1
Jorge García <jgarciao@gmail.com>
2017-09-07 16:29:00 +0200

    Improve AppListComponent (set column dynamically, fix css, ...)

--- src/app/pages/app-list/app-list.component.ts
+++ src/app/pages/app-list/app-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { App } from '../../shared/app.model';
-import { LinuxStoreApiService} from '../../linux-store-api.service';
+import { LinuxStoreApiService } from '../../linux-store-api.service';
 
 @Component({
   selector: 'linux-store-app-list',
@@ -14,42 +14,46 @@ export class AppListComponent implements OnInit {
   apps: App[];
   selectedApp: App;
   errorMessage: string;
+  numCols: number;
+  columnWidth: number = 160;
 
   constructor(
     private router: Router,
     private linuxStoreApiService: LinuxStoreApiService) {
   }
 
+  ngOnInit() {
+    this.getApps();
+    this.updateNumCols(innerWidth);
+  }
+
+  onResize(event) {
+    this.updateNumCols(event.target.innerWidth);
+  }
+
+  updateNumCols(width: number) {
+    this.numCols = Math.max(2, Math.floor(width / this.columnWidth));
+  }
+
   getApps(): void {
     this.linuxStoreApiService.getApps()
       .then(
-        apps => this.apps = apps,
-        error => this.errorMessage = <any> error);
-
-    console.log('getApps');
-
-  }
-
-  ngOnInit() {
-    this.getApps();
+      apps => this.apps = apps,
+      error => this.errorMessage = <any>error);
   }
 
   onSelect(app: App): void {
-    console.log('SelectedApp = ' + app.flatpakAppId);
     this.selectedApp = app;
   }
 
   gotoDetail(flatpakAppId: string): void {
-    this.router.navigate(['apps/details',flatpakAppId]);
+    this.router.navigate(['apps/details', flatpakAppId]);
   }
 
   isSelected(app: App): boolean {
-
     if (!app || !this.selectedApp) {
-       console.log('isSelected' + false);
       return false;
     }
-     console.log('isSelected' + (app.flatpakAppId === this.selectedApp.flatpakAppId));
     return app.flatpakAppId === this.selectedApp.flatpakAppId;
   }
 }
