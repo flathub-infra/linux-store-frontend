ccd2b41145a599ed5794b4f33b580eff22df99a3
Jorge García <jgarciao@gmail.com>
2017-12-10 11:36:27 +0100

    Add screenshots in app-details

--- src/app/shared/app-details-description/app-details-description.component.ts
+++ src/app/shared/app-details-description/app-details-description.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { Gallery, GalleryItem } from 'ng-gallery';
 
 import { App } from '../../shared/app.model';
 
@@ -8,13 +9,80 @@ import { App } from '../../shared/app.model';
   templateUrl: './app-details-description.component.html',
   styleUrls: ['./app-details-description.component.scss']
 })
-export class AppDetailsDescriptionComponent implements OnInit {
+export class AppDetailsDescriptionComponent implements AfterViewInit {
 
   @Input() app: App;
 
-  constructor() { }
+  constructor(public gallery: Gallery) {
+  }
 
-  ngOnInit() {
+ngAfterViewInit() {
+
+              const images: GalleryItem[] = [
+                {
+                  src: 'https://flathub.org/repo/screenshots/org.gnome.Builder-stable/624x351/org.gnome.Builder-3582853b1ad4a82da236a964983cef7c.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-f78bee1071a9541a87ba6d2153c9679f.png'
+                },
+                {
+                  src: 'https://flathub.org/repo/screenshots/org.gnome.Builder-stable/624x351/org.gnome.Builder-19b4818c4fda40f94d8e6ccc1379dc6d.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-877c35adb576a57d452d016b89778b62.png'
+                },
+                {
+                  src: 'https://flathub.org/repo/screenshots/org.gnome.Builder-stable/624x351/org.gnome.Builder-e8478cf3a7b4c87b6d793fa51c9af497.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-51d2d5d18af0fe46212c34be1b0c48f4.png'
+                }
+              ];
+
+              const images2: GalleryItem[] = [
+                {
+                  src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-f78bee1071a9541a87ba6d2153c9679f.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-f78bee1071a9541a87ba6d2153c9679f.png'
+                },
+                {
+                  src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-877c35adb576a57d452d016b89778b62.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-877c35adb576a57d452d016b89778b62.png'
+                },
+                {
+                  src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-51d2d5d18af0fe46212c34be1b0c48f4.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-51d2d5d18af0fe46212c34be1b0c48f4.png'
+                },
+                {
+                  src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-fec44e9e37f37de124cda149099818bf.png',
+                  thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-fec44e9e37f37de124cda149099818bf.png'
+                }
+              ];
+
+              const images3: GalleryItem[] = [];
+
+
+              this.gallery.load(images2);
+
+
+      // setTimeout(() => {
+
+      //   const images: GalleryItem[] = [
+      //     {
+      //       src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-f78bee1071a9541a87ba6d2153c9679f.png',
+      //       thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-f78bee1071a9541a87ba6d2153c9679f.png'
+      //     },
+      //     {
+      //       src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-877c35adb576a57d452d016b89778b62.png',
+      //       thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-877c35adb576a57d452d016b89778b62.png'
+      //     },
+      //     {
+      //       src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-51d2d5d18af0fe46212c34be1b0c48f4.png',
+      //       thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-51d2d5d18af0fe46212c34be1b0c48f4.png'
+      //     },
+      //     {
+      //       src: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/624x351/com.play0ad.zeroad-fec44e9e37f37de124cda149099818bf.png',
+      //       thumbnail: 'https://flathub.org/repo/screenshots/com.play0ad.zeroad-stable/224x126/com.play0ad.zeroad-fec44e9e37f37de124cda149099818bf.png'
+      //     }
+      //   ];
+
+      //   this.gallery.load(images);
+      // }, 0);
+
+
   }
 
 }
