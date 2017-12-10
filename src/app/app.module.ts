d906d4cc739a5c5e7df7b44ff0cffe1c61aeec9c
Jorge García <jgarciao@gmail.com>
2017-12-10 11:35:54 +0100

    Add hammerjs and ng-gallery dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LinuxStoreAngularMaterialModule } from './linux-store-angular-material/linux-store-angular-material.module';
+import { GalleryModule, GalleryConfig } from 'ng-gallery';
 
 import { routing } from './app.routing';
 import { AppComponent } from './app.component';
@@ -22,6 +23,38 @@ import { HomeComponent } from './pages/home/home.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { AppDetailsInstallInstructionsComponent } from './shared/app-details-install-instructions/app-details-install-instructions.component';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+
+export const galleryConfig: GalleryConfig = {
+  "gestures": true,
+  "style": {
+    "width": "900px",
+    "height": "460px",
+    //"background": "#121519"
+    "background": "#BCBCB6"
+  },
+  "navigation": {},
+  "loader": {
+    "icon": "data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='135' height='140' fill='%23fff'%3E%3Crect width='15' height='120' y='10' rx='6'%3E%3Canimate attributeName='height' begin='0.5s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite'/%3E%3Canimate attributeName='y' begin='0.5s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite'/%3E%3C/rect%3E%3Crect width='15' height='120' x='30' y='10' rx='6'%3E%3Canimate attributeName='height' begin='0.25s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite'/%3E%3Canimate attributeName='y' begin='0.25s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite'/%3E%3C/rect%3E%3Crect width='15' height='140' x='60' rx='6'%3E%3Canimate attributeName='height' begin='0s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite'/%3E%3Canimate attributeName='y' begin='0s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite'/%3E%3C/rect%3E%3Crect width='15' height='120' x='90' y='10' rx='6'%3E%3Canimate attributeName='height' begin='0.25s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite'/%3E%3Canimate attributeName='y' begin='0.25s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite'/%3E%3C/rect%3E%3Crect width='15' height='120' x='120' y='10' rx='6'%3E%3Canimate attributeName='height' begin='0.5s' dur='1s' values='120;110;100;90;80;70;60;50;40;140;120' calcMode='linear' repeatCount='indefinite'/%3E%3Canimate attributeName='y' begin='0.5s' dur='1s' values='10;15;20;25;30;35;40;45;50;0;10' calcMode='linear' repeatCount='indefinite'/%3E%3C/rect%3E%3C/svg%3E"
+  },
+  "description": null,
+  "player": {
+    "autoplay": false,
+    "interval": 3000,
+    "progress": true,
+    "position": "bottom"
+  },
+  "thumbnails": {
+    "width": 90,
+    "height": 70,
+    "position": "bottom"
+  },
+  "lightbox": {
+    "backdropClass": "g-backdrop",
+    "panelClass": "g-overlay",
+    "hasBackdrop": true
+  },
+  "imageSize": "cover"
+}
 
 @NgModule({
   declarations: [
@@ -45,6 +78,7 @@ import { ToolbarComponent } from './shared/toolbar/toolbar.component';
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    GalleryModule.forRoot(galleryConfig),
     FlexLayoutModule,
     LinuxStoreAngularMaterialModule,
     routing
