94413e7ae516ff6f6aeb97c45b879a268a6998d9
Jorge García <jgarciao@gmail.com>
2018-02-19 06:47:21 +0100

    Finally fix bug updating info in AppListComponent

--- src/app/pages/app-list/app-list.component.ts
+++ src/app/pages/app-list/app-list.component.ts
@@ -45,7 +45,7 @@ export class AppListComponent implements OnInit {
       params => {
         this.paramCategoryId = params.get('categoryId');
         this.paramCollectionId = params.get('collectionId');
-        if (this.isSmallScreen()) this.showAppsByParams();
+        if (this.isSmallScreen() || this.drawer.opened) this.showAppsByParams();
       }
     );
 
