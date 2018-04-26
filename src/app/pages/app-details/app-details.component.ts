009c903dd34a0e99dcf21991e7a3a851acf18c03
Jorge García <jgarciao@gmail.com>
2018-04-26 10:28:00 +0200

    Stop using FileSave.js in the INSTALL button

--- src/app/pages/app-details/app-details.component.ts
+++ src/app/pages/app-details/app-details.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
-import { saveAs } from "file-saver";
 
 import { App } from '../../shared/app.model';
 import { Review } from '../../shared/review.model';
@@ -73,45 +72,9 @@ export class AppDetailsComponent implements OnInit {
     return review.app_id === this.selectedReview.app_id;
   }
 
-  // This method has been adapted from Stack Overflow
-  // Question: http://stackoverflow.com/questions/35368633/angular-2-download-pdf-from-api-and-display-it-in-view
-  // Answer by spock: http://stackoverflow.com/users/435743/spock
-
   onInstall(app: App) {
-
-    // Track event
+    // Track instal event
     this.googleAnalyticsEventsService.emitEvent("App", "Install", app.flatpakAppId);
-
-    // Xhr creates new context so we need to create reference to this
-    let self = this;
-
-    // Status flag used in the template.
-    this.pending = true;
-
-    // Create the Xhr request object
-    let xhr = new XMLHttpRequest();
-    let url = this.app.downloadFlatpakRefUrl;
-
-    xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
-
-    // Xhr callback when we get a result back
-    // We are not using arrow function because we need the 'this' context
-    xhr.onreadystatechange = function () {
-
-      // We use setTimeout to trigger change detection in Zones
-      setTimeout(() => { self.pending = false; }, 0);
-
-      // If we get an HTTP status OK (200), save the file using fileSaver
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var blob = new Blob([this.response], { type: 'application/vnd.flatpak.ref' });
-        var filename: string = url.substring(url.lastIndexOf('/') + 1);
-        saveAs(blob, filename);
-      }
-    };
-
-    // Start the Ajax request
-    xhr.send();
   }
 
 
