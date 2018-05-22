4721aa6f805376478c708f8842a2f4493d9df106
Jorge García <jgarciao@gmail.com>
2018-05-22 06:46:08 +0200

    Fix javascript error when Google Analytics is not available

--- src/app/google-analytics-events.service.ts
+++ src/app/google-analytics-events.service.ts
@@ -2,20 +2,33 @@ import { Injectable } from '@angular/core';
 
 declare let ga: Function;
 
-// This method has been adapted from Stack Overflow
-// Question: https://stackoverflow.com/questions/45758852/angular-4-using-google-analytics/46463247#46463247
-// Answer by laiso: https://stackoverflow.com/users/8417384/laiso
-
 @Injectable()
 export class GoogleAnalyticsEventsService {
 
+  public emitPageView(url: string) {
+
+    if (typeof ga !== 'undefined') {
+      ga('set', 'page', url);
+      ga('send', 'pageview');
+    }
+
+  }
+
+
+  // This method has been adapted from Stack Overflow
+  // Question: https://stackoverflow.com/questions/45758852/angular-4-using-google-analytics/46463247#46463247
+  // Answer by laiso: https://stackoverflow.com/users/8417384/laiso
   public emitEvent(eventCategory: string, eventAction: string, eventLabel: string = null, eventValue: number = null) {
-    ga('send', 'event', {
-      eventCategory: eventCategory,
-      eventLabel: eventLabel,
-      eventAction: eventAction,
-      eventValue: eventValue
-    });
+
+    if (typeof ga !== 'undefined') {
+      ga('send', 'event', {
+        eventCategory: eventCategory,
+        eventLabel: eventLabel,
+        eventAction: eventAction,
+        eventValue: eventValue
+      });
+    }
+
   }
 
 }
