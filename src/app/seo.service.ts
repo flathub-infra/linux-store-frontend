1702a302554783553b725ca8adef349a00ce9964
Jorge García <jgarciao@gmail.com>
2018-06-01 16:31:14 +0200

    Seo rendering refactoring

--- src/app/seo.service.ts
+++ src/app/seo.service.ts
@@ -1,22 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Title, Meta } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class SeoService {
 
-  constructor(private titleService: Title,
+  constructor(
+    private router: Router,
+    private titleService: Title,
     private metaService: Meta) { }
 
-  setTitle(title: string) {
+  setPageMetadata(title: string = '', description: string = '', image: string = '') {
+
+    title = title + ' | Flathub';
+
     this.titleService.setTitle(title);
-  }
-
-  setDescription(description: string){
     this.metaService.updateTag({ name: 'description', content: description });
+
+    this.metaService.updateTag({ name: 'og:title', content: title });
+    this.metaService.updateTag({ name: 'og:type', content: 'website' });
+    this.metaService.updateTag({ name: 'og:image', content: image });
+    this.metaService.updateTag({ name: 'og:url', content: window.location.href });
+    this.metaService.updateTag({ name: 'og:description', content: description });
+
+    this.metaService.updateTag({ name: 'twitter:card', content: 'summary' });
+    this.metaService.updateTag({ name: 'twitter:title', content: title });
+    this.metaService.updateTag({ name: 'twitter:description', content: description });
+    this.metaService.updateTag({ name: 'twitter:image', content: image });
   }
 
-  setKeywords(keywords: string){
-    this.metaService.updateTag({ name: 'keywords', content: keywords });
-  }
-  
 }
