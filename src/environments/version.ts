1f7d7972c595c98e723c17dd6c91b0306237eeca
Jorge García <jgarciao@gmail.com>
2018-09-27 23:58:25 +0200

    Set version to 0.7.0

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "7bd3960",
-    "hash": "7bd3960",
+    "dirty": true,
+    "raw": "a3690c2-dirty",
+    "hash": "a3690c2",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "7bd3960",
+    "suffix": "a3690c2-dirty",
     "semverString": null,
-    "version": "0.7.0-alpha.4"
+    "version": "0.7.0"
 };
 /* tslint:enable */
