f1072e74235f1d51f69b56d798598fac585bdae3
nmanumr <u.nauman@gmail.com>
2018-11-30 17:25:30 +0500

    added little shadow under toolbar

--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 
 @Component({
@@ -6,12 +6,26 @@ import { Output, EventEmitter } from '@angular/core';
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
 
   @Output('search') search: EventEmitter<string> = new EventEmitter<string>();
 
   showSearchInput = false;
   searchActive = false;
+  isScrolled = false;
+
+  ngOnInit() {
+    document.addEventListener('scroll', () => {
+      var top = window.scrollY;
+
+      if (top > 30 && !this.isScrolled) {
+        this.isScrolled = true
+      }
+      else if (top < 30 && this.isScrolled) {
+        this.isScrolled = false;
+      }
+    })
+  }
 
   onSearch(searchTerm: string): void {
     this.showSearchInput = false;
