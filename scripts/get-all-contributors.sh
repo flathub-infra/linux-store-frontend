54ac55f1b78378d8e8e04cd6b1910ab218b7b169
Jorge García Oncins <jgarciao@gmail.com>
2019-04-10 06:56:49 +0200

    Improve scripts/get-all-contributors.sh

--- scripts/get-all-contributors.sh
+++ scripts/get-all-contributors.sh
@@ -13,9 +13,17 @@ fi
 GITHUB_ACCESS_TOKEN=$1
 APPIDLIST=`curl -s https://flathub.org/api/v1/apps | jq -r  .[].flatpakAppId | sort`
 
+echo "appid,contributor,order"
 
 for appid in $APPIDLIST  
 do
-  curl -s https://api.github.com/repos/flathub/$appid/contributors?access_token=$GITHUB_ACCESS_TOKEN | grep login  | cut -f 2 -d ":" | sed  "s/ \"/$appid:/" | sed 's/",//'
- 
+  CONTRIBUTORS=`curl -s https://api.github.com/repos/flathub/$appid/contributors?access_token=$GITHUB_ACCESS_TOKEN | jq -r .[].login`
+
+  let "i=1"
+  for contributorid in $CONTRIBUTORS
+  do
+	echo "$appid,$contributorid,$i"
+	let "i++"
+  done
+
 done
