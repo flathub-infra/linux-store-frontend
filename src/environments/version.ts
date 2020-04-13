7dfda8abb56da54771e67a9e29d84957678f934e
Jorge García Oncins <jgarciao@gmail.com>
2020-04-13 16:44:55 +0200

    Update version to 0.7.5

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,13 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "88c4e80-dirty",
-    "hash": "88c4e80",
+    "raw": "d929544-dirty",
+    "hash": "d929544",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "88c4e80-dirty",
+    "suffix": "d929544-dirty",
     "semverString": null,
-    "version": "0.7.4"
+    "version": "0.7.5"
 };
 /* tslint:enable */
