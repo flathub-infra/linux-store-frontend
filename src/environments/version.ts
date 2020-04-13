707e15139403c690fc45ea93db5009777a6d259e
Jorge García Oncins <jgarciao@gmail.com>
2020-04-13 16:50:24 +0200

    Update vesion file again

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "d929544-dirty",
-    "hash": "d929544",
+    "dirty": false,
+    "raw": "a8524a1",
+    "hash": "a8524a1",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "d929544-dirty",
+    "suffix": "a8524a1",
     "semverString": null,
     "version": "0.7.5"
 };
