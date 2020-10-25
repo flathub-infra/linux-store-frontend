4e2e9024b6d9a3a59551c6e1fb59278937278155
itea.dev <itea.dev@outlook.com>
2020-10-25 14:14:52 +0100

    Remove unused imports

--- src/app/linux-store-api.service.ts
+++ src/app/linux-store-api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
